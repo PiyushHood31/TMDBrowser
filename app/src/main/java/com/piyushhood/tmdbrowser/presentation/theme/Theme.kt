9ee02698294209933d8a35348264fb9000d8d5e1
Piyush-Logituit <piyush.hood@logituit.com>
2026-01-10 15:15:11 +0530

    Phase 1.5: Apply dynamic theme based on user preference

--- app/src/main/java/com/piyushhood/tmdbrowser/presentation/theme/Theme.kt
+++ app/src/main/java/com/piyushhood/tmdbrowser/presentation/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.piyushhood.tmdbrowser.domain.model.ThemeMode
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -34,23 +35,17 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun TMDBrowserTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    themeMode: ThemeMode,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val darkTheme = when (themeMode){
+        ThemeMode.LIGHT -> false
+        ThemeMode.DARK -> true
+        ThemeMode.SYSTEM ->isSystemInDarkTheme()
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = if(darkTheme) DarkColorScheme else LightColorScheme,
         typography = Typography,
         content = content
     )
