2b860d1ea431d48d29f1a5f76a867069aadd28cb
Piyush-Logituit <piyush.hood@logituit.com>
2026-01-12 19:31:54 +0530

    Phase 3.1: Setup TMDB API networking foundation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
 }
 
 android {
@@ -16,6 +17,12 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
+
+        buildConfigField(
+            "String",
+            "TMDB_API_KEY",
+            "\"${project.findProperty("TMDB_API_KEY")}\""
+        )
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -38,6 +45,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -62,6 +70,9 @@ dependencies {
     implementation(libs.androidx.material3.window.size)
     implementation(libs.androidx.datastore.preferences)
 
-
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlinx.serialization)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
 
 }
---
+++ app/src/main/java/com/piyushhood/tmdbrowser/data/remote/RetrofitProvider.kt
@@ -0,0 +1,24 @@
+package com.piyushhood.tmdbrowser.data.remote
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+@OptIn(ExperimentalSerializationApi::class)
+fun provideRetrofit(okHttpClient: OkHttpClient) : Retrofit {
+    val json = Json{
+        ignoreUnknownKeys = true
+        isLenient = true
+    }
+
+    return Retrofit.Builder()
+        .baseUrl(TmdbConstants.BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(
+            json.asConverterFactory("application/json".toMediaType())
+        )
+        .build()
+}
