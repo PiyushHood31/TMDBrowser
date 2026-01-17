e6a286d2aebb21ccfe2e5fa3a9578a72301db551
Piyush-Logituit <piyush.hood@logituit.com>
2026-01-17 14:58:29 +0530

    Phase 5.4: Add Room database and Hilt database module

---
+++ app/src/main/java/com/piyushhood/tmdbrowser/data/local/database/AppDatabase.kt
@@ -0,0 +1,15 @@
+package com.piyushhood.tmdbrowser.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.piyushhood.tmdbrowser.data.local.dao.MovieDao
+import com.piyushhood.tmdbrowser.data.local.entity.MovieEntity
+
+@Database(
+    entities = [MovieEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun movieDao() : MovieDao
+}
